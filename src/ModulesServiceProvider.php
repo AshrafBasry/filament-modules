e04e0a23143f91255db547a3a933e85cc0aa5e7d
Ashraf Basry <ashrafmn2008@gmail.com>
2024-01-27 03:25:28 +0200

    Fix config back title

--- src/ModulesServiceProvider.php
+++ src/ModulesServiceProvider.php
@@ -53,7 +53,7 @@ class ModulesServiceProvider extends PackageServiceProvider
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
                                             </svg>
-                                           ' . __(config('nav_end_back_title')) . '
+                                           ' . __(config('modules.nav_end_back_title')) . '
                                         </a>'
                                 ),
                             );
